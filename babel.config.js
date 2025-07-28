a2916d6f3f9dba6da0368ea501a0b8b930af29b9
Nelson Rosales <56582884+nelson2411@users.noreply.github.com>
2025-07-28 07:44:44 +0200

    refactor: remove .babelrc and consolidate Babel configuration in babel.config.js fix: update Node.js engine version in package.json to >=16

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '16',
+        },
+      },
+    ],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+  ],
 };
